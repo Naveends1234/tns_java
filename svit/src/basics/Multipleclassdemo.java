b25da123ab3c0da8aec21a59dcfb7ccdd4af14d3
Naveends1234 <dsnaveen08@gmail.com>
2026-02-11 12:22:12 +0530

    message

---
+++ svit/src/basics/Multipleclassdemo.java
@@ -0,0 +1,42 @@
+package basics;
+
+class Employee {
+    int empId;
+    String empName;
+
+    void setEmployee(int id, String name) {
+        empId = id;
+        empName = name;
+    }
+
+    void displayEmployee() {
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Employee Name: " + empName);
+    }
+}
+
+class Department {
+    String deptName;
+
+    void setDepartment(String dept) {
+        deptName = dept;
+    }
+
+    void displayDepartment() {
+        System.out.println("Department: " + deptName);
+    }
+}
+
+public class Multipleclassdemo {
+	 public static void main(String[] args) {
+
+	        Employee e1 = new Employee();
+	        e1.setEmployee(101, "Naveen");
+	        
+	        Department d1 = new Department();
+	        d1.setDepartment("IT");
+
+	        e1.displayEmployee();
+	        d1.displayDepartment();
+	    }
+	}
